c4740a40e3b222d3d3563ffc72a082bd032df386
gab James <jam12012@byui.edu>
2014-10-06 22:30:44 -0500

    Initial Commit

---
+++ walkTheDog/src/walkthedog/WalkTheDog.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package walkthedog;
+
+import byui.cit260.walkTheDog.model.Player;
+
+/**
+ *
+ * @authors Idel Pagan and gab James
+ */
+public class WalkTheDog {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Player playerOne = new Player();
+        
+        playerOne.setName("Fred Flintsone");
+        playerOne.setBestTime(7.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+    }
+    
+}
