ce5cacb4e3fdbb98a8d270dc6be009268bfedd5e
gab James <jam12012@byui.edu>
2014-10-21 00:40:27 -0500

    gab added Control class EventControl_gab

--- walkTheDog/src/byui/cit260/walkTheDog/control/EventControlIdel.java
+++ walkTheDog/src/byui/cit260/walkTheDog/control/EventControlIdel.java
@@ -9,6 +9,14 @@ package byui.cit260.walkTheDog.control;
  *
  * @author Idel
  */
+
+
+// I fixed teh code and added it to the bottom
+// try to see if you can fix yours but if you 
+//really get to a stumbling place scroll down and see what I did.
+
+
+
 public class EventControlIdel {
     public int generateFidoMood(int numberOfTurns, int leashLength);
     
@@ -27,3 +35,56 @@ public class EventControlIdel {
     
    
 }
+
+
+// This code is fixed and i belive is correct 
+// Only scroll down if you really need a help to find what is wrong
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//public class EventControlIdel {
+//    there was a { here that closed the class
+//
+//
+//    public int generateFidoMood(int numberOfTurns, int leashLength, int mood){
+//
+//    int mood must go in the called line to pass the current mood into the function 
+//    otherwise the mood always starts out at 0
+//    
+//    if (leashLength <= 4 && numberOfTurns > 1){
+//    mood =- 2; // fido is mad
+//    }                                     // must open and close an if with { }
+//    
+//    if (mood <= 0){
+//    return -1; //lose life point!
+//    }
+//    
+//    else if (leashLength > 4){
+//    mood =+ 1; //fido is happy
+//    }
+//    
+//    return mood;
+//
+//    
+//   
+//    }       // this { was missing
+//}
