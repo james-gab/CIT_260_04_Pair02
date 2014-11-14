4c4e26a90e3a2d1b67a5bed235c672de02ba1139
gab James <jam12012@byui.edu>
2014-11-14 00:11:50 -0600

    gab

--- walkTheDog/src/byui/cit260/walkTheDog/model/Location.java
+++ walkTheDog/src/byui/cit260/walkTheDog/model/Location.java
@@ -15,7 +15,7 @@ import java.util.Objects;
 public class Location implements Serializable {
     private int row; 
     private int column;
-    private String visited; 
+    private boolean visited; 
     private String explore; 
     private String randomEvent;
     private String actorRandomEvent;
@@ -39,11 +39,11 @@ public class Location implements Serializable {
         this.column = column;
     }
 
-    public String getVisited() {
+    public boolean getVisited() {
         return visited;
     }
 
-    public void setVisited(String visited) {
+    public void setVisited(boolean visited) {
         this.visited = visited;
     }
 
