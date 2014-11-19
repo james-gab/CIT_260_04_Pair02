bfdd642ab7942f016d7362dbd44f69c2fb42608a
gab James <jam12012@byui.edu>
2014-11-18 18:23:54 -0600

    gab fixed variables after Lesson 08 upload

--- walkTheDog/src/byui/cit260/walkTheDog/model/ExploringEvents.java
+++ walkTheDog/src/byui/cit260/walkTheDog/model/ExploringEvents.java
@@ -6,6 +6,7 @@
 package byui.cit260.walkTheDog.model;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.Objects;
 
 /**
@@ -13,13 +14,63 @@ import java.util.Objects;
  * @author Idel
  */
 public class ExploringEvents implements Serializable {
+    private int row; 
+    private int column;
+    private boolean visited; 
     private String eventName; 
     private String evenDescription;
     private String criteria;
+    public String [][] exploringEvent = new String [row][column] ;
+    private int noOfRows;
+    private int noOfColums;
+//    public Object exploringEvent;
 
     public ExploringEvents() {
     }
+    
+    
 
+    
+    
+    
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public boolean getVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
     
     public String getEventName() {
         return eventName;
@@ -79,6 +130,40 @@ public class ExploringEvents implements Serializable {
         }
         return true;
     }
+    
+    
+    
+    
+    public ExploringEvents(int noOfRows, int noOfColums) {
+        
+        if (noOfRows<1 || noOfColums<1){
+            System.out.println("*** Our appologies, something went wrong. ***"
+                    + "\n*** ERROR in GameMenuView.java in ***"
+                    + "\n   ExploringEvents() if (noOfRows<1 || noOfColums<1)***");
+            return;
+        }
+        String [][] exploringEvent = new String [noOfRows][noOfColums] ;
+        this.noOfRows = noOfRows;
+        this.noOfColums = noOfColums;
+        
+        for (int row1 = 0; row1 < noOfRows; row1++){
+            for (int column1 = 0; column1 < noOfColums; column1++){
+                String [][] eEvent = new ;
+                eEvent = this.setColumn(column1);
+                String [][] exploringEvent.this.setRow(row1);
+                String [][] exploringEvent.this.setVisited(false);
+                
+                //assign the Location object to the current position in array
+                        exploringEvents[row1][column1] = exploringEvent;
+            }
+        }
+        
+        
+    }
+
+    
+    
+    
     
     
 }
