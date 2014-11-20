921ce25bfec9930f08e0b6005ac4bfa977ec79c3
gab James <jam12012@byui.edu>
2014-11-19 22:48:51 -0600

    Merge origin/master
    Conflicts:
    	walkTheDog/src/byui/cit260/walkTheDog/control/ExploringEventsControl.java
    	walkTheDog/src/byui/cit260/walkTheDog/control/GameControl.java

--- walkTheDog/src/byui/cit260/walkTheDog/control/MiniGameControl.java
+++ walkTheDog/src/byui/cit260/walkTheDog/control/MiniGameControl.java
@@ -27,22 +27,23 @@ public class MiniGameControl {
                 + "\nYou get 3 chances!"
                 + "\nIf you win, you get an extra life!");
        
-        do
-        {
+        do{
          
         
             System.out.println("Press any key to spin the first number!");
             Scanner keyboard = new Scanner(System.in);                          //user can enter any key to continue
             keyboard.nextLine();
             Random rand = new Random();
+
             for (int i = 0; i < randomNumbers1.length; i++) {                   //for loop adds random numbers to the first array
                 int spin1 = rand.nextInt(10);
                 randomNumbers1[i] = spin1;
                }
+
             int random1 = (randomNumbers1[new Random().nextInt(randomNumbers1.length)]);    //a random number is selected from the array
             System.out.println("\nYour first number is: " + random1 + "!");
        
-            System.out.println("\nPress any key to spin the second number!");
+            System.out.println("\n\nPress any key to spin the second number!");
             Scanner keyboard1 = new Scanner(System.in);                         //user can enter any key to continue
                 keyboard1.nextLine();            
         
@@ -54,16 +55,14 @@ public class MiniGameControl {
             System.out.println("\nYour second number is: " + random2 + "!");
         
             int totalNum = (random1 + random2);                                 //the two random numbers selected from the array are added
+            
             System.out.println(random1 + " + " + random2 + " = " + totalNum + "!");
+
             if (totalNum < 10){                                                 //checks if the added number is less than 10
                 System.out.println("\nSorry! Your total number is less than 10!");
                 number--;
-                System.out.println("\nYou have " + number + " chances left."
+                System.out.println("\nYou have " + number + " chance(s) left."
                               + "\nBetter luck next time!");
-                System.out.println("\nPress any key to return to play again!");
-                Scanner keyboard3 = new Scanner(System.in);
-                keyboard.nextLine();
-              
             }
             else {                                                              //if the two added numbers are greated than 10 then user recieves an extra life point
                 System.out.println("\nCongratulations! You recieved an extra life point!");
@@ -75,6 +74,16 @@ public class MiniGameControl {
          
                 return lifePoint;
             } 
+            
+                System.out.println("\nWould you like to spin again? (Y/N)");
+                Scanner keyboard3 = new Scanner(System.in);
+                keyboard.nextLine();
+                
+                
+                
+                if (keyboard3.equals("Y")){
+              
+            
         }while (number != 0);
         
         System.out.println("\nSorry, you either already revieved an extra life" //this part is displayed if the user already recieved a life point
---
+++ walkTheDog/src/byui/cit260/walkTheDog/view/MiniGameView.java
@@ -0,0 +1,31 @@
+/*
+ * Project author: gab James.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.walkTheDog.view;
+
+/**
+ *
+ * @author gab
+ */
+public class MiniGameView extends View {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
